037b063ebb50c9f502972a5d42f259ffcb8b7390
Nguyenanvu202 <106262321+Nguyenanvu202@users.noreply.github.com>
2025-11-19 13:39:50 +0700

    Update Last_Checkin

--- PC information.bat
+++ PC information.bat
@@ -49,7 +49,27 @@ if not defined GPU_NAME (
     )
 )
 
+:: Get current timestamp
+:: Use WMIC to retrieve date and time
+FOR /F "skip=1 tokens=1-6" %%G IN ('WMIC Path Win32_LocalTime Get Day^,Hour^,Minute^,Month^,Second^,Year /Format:table') DO (
+   IF "%%~L"=="" goto s_done
+      Set _yyyy=%%L
+      Set _mm=00%%J
+      Set _dd=00%%G
+      Set _hour=00%%H
+      SET _minute=00%%I
+)
+:s_done
 
+:: Pad digits with leading zeros
+      Set _mm=%_mm:~-2%
+      Set _dd=%_dd:~-2%
+      Set _hour=%_hour:~-2%
+      Set _minute=%_minute:~-2%
+
+:: Display the date/time in ISO 8601 format:
+Set LAST_CHECKIN=%_yyyy%-%_mm%-%_dd% %_hour%:%_minute%
+Echo %_isodate%
 :: Output
 echo ========================================
 echo System Information
@@ -58,6 +78,7 @@ echo Operating System: %OS_NAME% (%OS_VERSION%)
 echo Processor:        %CPU%
 echo RAM:              %TotalPhysicalMemory% GB
 echo GPU NAME:          %GPU_NAME%
+echo Last Check-in: %LAST_CHECKIN%
 echo ========================================
 echo.
 echo IPs detected successfully.
